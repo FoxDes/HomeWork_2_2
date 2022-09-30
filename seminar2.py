ba0bd89d6b1f2a0411ebb40b8bb7eedd96584b99
Denis Malyarenko <mdn-fox@yandex.ru>
2022-09-30 16:17:11 +0500

    Выполнил третью задачу, это было головоломно

---
+++ seminar2.py
@@ -0,0 +1,25 @@
+# Возведение в степень -3
+
+# N = int(input('Введи число: '))
+# num = 1
+# for i in range(N):
+#     print(num, end=" ")
+#     num = num * (-3)
+
+
+# N = int(input('Введи число: '))
+# ls = list()
+# for k in range(1,N+1):
+#     ls.append(3*k+1)
+# print(ls)
+
+
+
+# # метод строк, найти повторяющиеся элементы в первой строке
+# str1 = input()
+# str2 = input()
+# print(str1.count(str2))
+
+
+
+
